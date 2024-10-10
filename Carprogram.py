7355c33b492fe7296d7901b743426e43dd50bf02
aljo6-albert <aljo6@student.eadania.dk>
2024-10-10 12:21:08 +0200

    opdateret

--- Carprogram.py
+++ Carprogram.py
@@ -14,7 +14,7 @@ PWM_B1 = 13  # PWM 1 for Motor B
 PWM_B2 = 26  # PWM 2 for Motor B
 
 # Initialize GPIO
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 
 # Initialize the PWM for motors
 GPIO.setup(PWM_A1, GPIO.OUT)
