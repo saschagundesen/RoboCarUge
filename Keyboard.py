418b7eef72f6632297f57c1b962ef2795c33873a
saschagundesen <sagu5@student.eadania.dk>
2024-10-11 12:48:03 +0200

    ny

--- Keyboard.py
+++ Keyboard.py
@@ -88,16 +88,28 @@ def move(state, speedleft, speedright):
 
 
 def GoForward():
- print('Going Forward')
+    print('Going Forward')
+    motor_A(True, 50)  # Kører fremad 50% speed
+    motor_B(True, 50)
+
 
 def GoBackward():
     print('Going Backward')
+    motor_A(False, 50)  # Kører fremad 50% speed
+    motor_B(False, 50)
+
     
 def TurnRight():
     print('Going Right')
+    motor_A(True, 50)  # Kører fremad 50% speed
+    motor_B(False, 50)
+
 
 def TurnLeft():
     print('Going Left')
+    motor_A(False, 50)  # Kører fremad 50% speed
+    motor_B(True, 50)
+
 
 def press(key):
     if key == "w":
