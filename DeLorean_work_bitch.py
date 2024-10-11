9697631ca5ed8b7e8f61c351e31efd69b81b5195
ana007nasik <anastasiamiam0@gmail.com>
2024-10-11 09:25:53 +0200

    aaaaaaaaaaa

--- DeLorean_work_bitch.py
+++ DeLorean_work_bitch.py
@@ -117,24 +117,24 @@ def move(state,speedleft,speedright):
 def on_line_A():
         print("Sensor A: Line detected! Adjusting motors.")
         # Motor A continues forward, Motor B slows down or adjusts
-        motor_A(True, False, 50)  # Move Motor A forward at 50% speed
-        motor_B(True, False, 15)  # Slow Motor B to turn towards the line
+        motor_A(True, False, 80)  # Move Motor A forward at 50% speed
+        motor_B(True, False, 20)  # Slow Motor B to turn towards the line
 
 def off_line_A():
         print("Sensor A: Off the line! Adjusting motors.")
         # Adjust motors when the sensor loses the line
         motor_A(True, False, 0)  # Stop Motor A
-        motor_B(True, False, 50)  # Speed up Motor B to adjust course
+        motor_B(True, False, 80)  # Speed up Motor B to adjust course
 
 def on_line_B():
         print("Sensor B: Line detected! Adjusting motors.")
         # Motor B continues forward, Motor A slows down or adjusts
-        motor_A(True, False, 0)  # Slow Motor A to turn towards the line
-        motor_B(True, False, 0)  # Move Motor B forward at 50% speed
+        motor_A(True, False, 20)  # Slow Motor A to turn towards the line
+        motor_B(True, False, 80)  # Move Motor B forward at 50% speed
 
 def off_line_B():
         print("Sensor B: Off the line! Adjusting motors.")
-        motor_A(True, False, 50)  # Speed up Motor A to adjust course
+        motor_A(True, False, 80)  # Speed up Motor A to adjust course
         motor_B(True, False, 0)  # Stop Motor B
 
 last_detection_A = time()
@@ -164,7 +164,10 @@ sensor_B.when_no_line = off_line_B
 
 try:
     while True:
-        move(GPIO.LOW,50,50)
+        move(80)
+        print("Motor speed:",PWM_A1_pwm.ChangeDutyCycle(80))
+        print("Motor direction:",GPIO.output(DIR_A1,GPIO.HIGH))
+        sleep(0.1)
        
 except KeyboardInterrupt:
     print('Programmet er stoppet')
