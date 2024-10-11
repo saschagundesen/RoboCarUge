b8335127b4f3e367d09ead350c1edc62b9adf120
saschagundesen <sagu5@student.eadania.dk>
2024-10-11 13:10:51 +0200

    ahha

--- Keyboard.py
+++ Keyboard.py
@@ -89,8 +89,8 @@ def move(state, speedleft, speedright):
 
 def GoForward():
     print('Going Forward')
-    motor_A(True,True, 50)  # Kører fremad 50% speed
-    motor_B(False,True, 50)
+    motor_A(False,False, 50)  # Kører fremad 50% speed
+    motor_B(True,False, 50)
 
 
 def GoBackward():
